2990f2605c271ee6898a4e8f656df953fed74566
Fatima Ferreira <ifatinha99@gmail.com>
2023-09-29 20:36:49 -0300

    testes

--- class-de-heroi/script.js
+++ class-de-heroi/script.js
@@ -39,3 +39,12 @@ class Heroi {
 
 const mago = new Heroi("Gandalf", 2000, "mago");
 mago.atacar();
+
+const guerreiro = new Heroi("Thorak", 35, "guerreiro");
+guerreiro.atacar();
+
+const monge = new Heroi("Samsara", 458, "monge");
+monge.atacar();
+
+const ninja = new Heroi("Azumi", 45, "ninja");
+ninja.atacar();
